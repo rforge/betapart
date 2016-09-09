68e411b35c2926704f20054b5f927d488551e3d4
abaselga <abaselga@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2016-09-09 09:41:09 +0000

    New abundance-based multiple-site functions
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/betapart@101 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/R/betapart-core-abund.R
@@ -0,0 +1,45 @@
+betapart.core.abund <- function(x){
+	
+	# test for a numeric matrix or data.frame
+	if(! is.matrix(x)){
+		x<-as.matrix(x)
+  	}
+	
+	if(! is.numeric(x))
+    	stop("The data in x is not numeric.",call.=TRUE)
+	
+	# simple test for positive data
+	xvals <-  unique(as.vector(x))
+	if (any(xvals<0)) 
+        stop("The table contains negative values: data should be abundances.", call. = TRUE)
+
+	pair.shared<-matrix(nrow=nrow(x),ncol=nrow(x))
+	rownames(pair.shared)<-rownames(x)
+	colnames(pair.shared)<-rownames(x)
+
+	pair.not.shared<-matrix(nrow=nrow(x),ncol=nrow(x))
+	rownames(pair.not.shared)<-rownames(x)
+	colnames(pair.not.shared)<-rownames(x)
+
+	for(i in 1:nrow(x)) {
+    	for(j in i:nrow(x)) {
+		pair.shared[j,i]<-sum(pmin(x[i,],x[j,]))
+		pair.not.shared[i,j]<-sum(x[i,])-sum(pmin(x[i,],x[j,]))
+		pair.not.shared[j,i]<-sum(x[j,])-sum(pmin(x[i,],x[j,]))
+		}
+		}
+
+	pair.shared<-as.dist(pair.shared)
+	pair.max.not.shared<-pmax(as.dist(t(upper.tri(pair.not.shared)*pair.not.shared)), as.dist(pair.not.shared))
+	pair.min.not.shared<-pmin(as.dist(t(upper.tri(pair.not.shared)*pair.not.shared)), as.dist(pair.not.shared))
+
+	multiple.shared<-sum(x)-sum(apply(x,2,max))
+
+
+
+	computations<-list(data=x, multiple.shared=multiple.shared, pair.shared=pair.shared, pair.max.not.shared=pair.max.not.shared, 
+	                   pair.min.not.shared=pair.min.not.shared, pair.sum.not.shared=pair.min.not.shared+pair.max.not.shared)
+    class(computations)<-"betapart.abund"
+
+	return(computations)
+} 
