d3990649e8e78261ce126a8028a614b05aa45c2d
abaselga <abaselga@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2016-09-23 08:52:33 +0000

    Corrected errors in abund functions
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/betapart@114 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/beta-pair-abund.R
+++ pkg/R/beta-pair-abund.R
@@ -6,20 +6,20 @@ beta.pair.abund<-function (x, index.family = "bray")
         x <- betapart.core.abund(x)
     }
     switch(index.family, bray = {
-        beta.bray.bal <- x$pair.min.not.shared/(x$pair.min.not.shared + x$pair.shared)
-        beta.bray.gra <- ((x$pair.max.not.shared - x$pair.min.not.shared)/((2 * 
-            x$pair.shared) + x$pair.sum.not.shared)) * (x$pair.shared/(x$pair.min.not.shared + 
-            x$pair.shared))
-        beta.bray <- x$pair.sum.not.shared/(2 * x$pair.shared + x$pair.sum.not.shared)
+        beta.bray.bal <- x$pair.min.not.shared.abund/(x$pair.min.not.shared.abund + x$pair.shared.abund)
+        beta.bray.gra <- ((x$pair.max.not.shared.abund - x$pair.min.not.shared.abund)/((2 * 
+            x$pair.shared.abund) + x$pair.sum.not.shared.abund)) * (x$pair.shared.abund/(x$pair.min.not.shared.abund + 
+            x$pair.shared.abund))
+        beta.bray <- x$pair.sum.not.shared.abund/(2 * x$pair.shared.abund + x$pair.sum.not.shared.abund)
         pairwise <- list(beta.bray.bal = as.dist(beta.bray.bal), beta.bray.gra = as.dist(beta.bray.gra), 
             beta.bray = as.dist(beta.bray))
     }, ruzicka = {
-        beta.ruz.bal <- (2 * x$pair.min.not.shared)/((2 * x$pair.min.not.shared) + 
-            x$pair.shared)
-        beta.ruz.gra <- ((x$pair.max.not.shared - x$pair.min.not.shared)/(x$pair.shared + 
-            x$pair.sum.not.shared)) * (x$pair.shared/((2 * x$pair.min.not.shared) + 
-            x$pair.shared))
-        beta.ruz <- x$pair.sum.not.shared/(x$pair.shared + x$pair.sum.not.shared)
+        beta.ruz.bal <- (2 * x$pair.min.not.shared.abund)/((2 * x$pair.min.not.shared.abund) + 
+            x$pair.shared.abund)
+        beta.ruz.gra <- ((x$pair.max.not.shared.abund - x$pair.min.not.shared.abund)/(x$pair.shared.abund + 
+            x$pair.sum.not.shared.abund)) * (x$pair.shared.abund/((2 * x$pair.min.not.shared.abund) + 
+            x$pair.shared.abund))
+        beta.ruz <- x$pair.sum.not.shared.abund/(x$pair.shared.abund + x$pair.sum.not.shared.abund)
         pairwise <- list(beta.ruz.bal = as.dist(beta.ruz.bal), beta.ruz.gra = as.dist(beta.ruz.gra), 
             beta.ruz = as.dist(beta.ruz))
     })
--- pkg/R/betapart-core-abund.R
+++ pkg/R/betapart-core-abund.R
@@ -13,32 +13,32 @@ betapart.core.abund <- function(x){
 	if (any(xvals<0)) 
         stop("The table contains negative values: data should be abundances.", call. = TRUE)
 
-	pair.shared<-matrix(nrow=nrow(x),ncol=nrow(x))
-	rownames(pair.shared)<-rownames(x)
-	colnames(pair.shared)<-rownames(x)
+	pair.shared.abund<-matrix(nrow=nrow(x),ncol=nrow(x))
+	rownames(pair.shared.abund)<-rownames(x)
+	colnames(pair.shared.abund)<-rownames(x)
 
-	pair.not.shared<-matrix(nrow=nrow(x),ncol=nrow(x))
-	rownames(pair.not.shared)<-rownames(x)
-	colnames(pair.not.shared)<-rownames(x)
+	pair.not.shared.abund<-matrix(nrow=nrow(x),ncol=nrow(x))
+	rownames(pair.not.shared.abund)<-rownames(x)
+	colnames(pair.not.shared.abund)<-rownames(x)
 
 	for(i in 1:nrow(x)) {
     	for(j in i:nrow(x)) {
-		pair.shared[j,i]<-sum(pmin(x[i,],x[j,]))
-		pair.not.shared[i,j]<-sum(x[i,])-sum(pmin(x[i,],x[j,]))
-		pair.not.shared[j,i]<-sum(x[j,])-sum(pmin(x[i,],x[j,]))
+		pair.shared.abund[j,i]<-sum(pmin(x[i,],x[j,]))
+		pair.not.shared.abund[i,j]<-sum(x[i,])-sum(pmin(x[i,],x[j,]))
+		pair.not.shared.abund[j,i]<-sum(x[j,])-sum(pmin(x[i,],x[j,]))
 		}
 		}
 
-	pair.shared<-as.dist(pair.shared)
-	pair.max.not.shared<-pmax(as.dist(t(upper.tri(pair.not.shared)*pair.not.shared)), as.dist(pair.not.shared))
-	pair.min.not.shared<-pmin(as.dist(t(upper.tri(pair.not.shared)*pair.not.shared)), as.dist(pair.not.shared))
+	pair.shared.abund<-as.dist(pair.shared.abund)
+	pair.max.not.shared.abund<-pmax(as.dist(t(upper.tri(pair.not.shared.abund)*pair.not.shared.abund)), as.dist(pair.not.shared.abund))
+	pair.min.not.shared.abund<-pmin(as.dist(t(upper.tri(pair.not.shared.abund)*pair.not.shared.abund)), as.dist(pair.not.shared.abund))
 
-	multiple.shared<-sum(x)-sum(apply(x,2,max))
+	multiple.shared.abund<-sum(x)-sum(apply(x,2,max))
 
 
 
-	computations<-list(data=x, multiple.shared.abund=multiple.shared, pair.shared.abund=pair.shared, pair.min.not.shared.abund=pair.min.not.shared, 
-		pair.max.not.shared.abund=pair.max.not.shared, pair.not.shared.abund=pair.min.not.shared+pair.max.not.shared)
+	computations<-list(data=x, multiple.shared.abund=multiple.shared.abund, pair.shared.abund=pair.shared.abund, pair.min.not.shared.abund=pair.min.not.shared.abund, 
+		pair.max.not.shared.abund=pair.max.not.shared.abund, pair.sum.not.shared.abund=pair.min.not.shared.abund+pair.max.not.shared.abund)
     class(computations)<-"betapart.abund"
 
 	return(computations)
